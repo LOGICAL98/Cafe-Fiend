3fd989e08e6295be11b16179d795c83107b17977
LOGICAL98 <67430411+LOGICAL98@users.noreply.github.com>
2022-05-25 20:14:45 -0400

    not finished

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
-export default function Login(){
-    return(
+export default function Login() {
+    return (
         <div className="login-wrapper">
             <h1>Please Log In</h1>
-        <form>
-            <label>
-                <p>Username</p>
-                <input type='text'/> 
-            </label>
-            <label>
-                <p>Password</p>
-                <input type="password" />
-            </label>
-            <div>
-                <button type="submit">Submit</button>
-                <button type="create account">Create Account</button>
-            </div>
-        </form>
+            <form>
+                <label>
+                    <p>Username</p>
+                    <input type='text' />
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" />
+                </label>
+                <div>
+                    <a href='/coffee'>
+                        <button type="submit">Submit</button>
+                    </a>
+                    <button type="create account">Create Account</button>
+                </div>
+            </form>
         </div>
     )
 }
